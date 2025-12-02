1a02a37a30e77e905fdf989b0c81f18c083c9865
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2025-12-02 21:04:12 +0530

    commit

--- src/components/gallery/MasonryGrid.tsx
+++ src/components/gallery/MasonryGrid.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Masonry from "react-masonry-css";
-import { SmartImage, SmartLink, Flex, Heading, Text } from "@/once-ui/components";
+import { SmartImage, SmartLink, Flex, Heading, Text, Avatar } from "@/once-ui/components";
 import styles from "./Gallery.module.scss";
-import { projects } from "@/app/resources/content";
+import { projects, person } from "@/app/resources/content";
 
 export default function MasonryGrid() {
   const breakpointColumnsObj = {
@@ -42,30 +42,71 @@ export default function MasonryGrid() {
             <Heading as="h3" variant="heading-strong-m">
               {project.title}
             </Heading>
+            
+            <Flex direction="column" gap="8">
+                {project.type && (
+                    <Text variant="label-default-s" onBackground="neutral-medium">
+                        Type: {project.type}
+                    </Text>
+                )}
+                {project.tech && (
+                    <Text variant="label-default-s" onBackground="neutral-medium">
+                        Tech: {project.tech}
+                    </Text>
+                )}
+            </Flex>
+
             {project.description && (
               <Text variant="body-default-s" onBackground="neutral-weak">
                 {project.description}
               </Text>
             )}
-            <Flex fillWidth horizontal="space-between" gap="24" wrap>
-              {project.caseStudy && (
-                <SmartLink
-                  suffixIcon="arrowRight"
-                  style={{ margin: "0", width: "fit-content" }}
-                  href={project.caseStudy}
-                >
-                  <Text variant="body-default-s">Read case study</Text>
-                </SmartLink>
-              )}
-              {project.link && (
-                <SmartLink
-                  suffixIcon="arrowUpRightFromSquare"
-                  style={{ margin: "0", width: "fit-content" }}
-                  href={project.link}
-                >
-                  <Text variant="body-default-s">View project</Text>
-                </SmartLink>
-              )}
+
+            {project.features && project.features.length > 0 && (
+                <Flex direction="column" gap="4">
+                    <Text variant="label-default-s" onBackground="neutral-strong">Features:</Text>
+                    <ul style={{ paddingLeft: '20px', margin: 0 }}>
+                        {project.features.map((feature, i) => (
+                            <li key={i}>
+                                <Text variant="body-default-s" onBackground="neutral-weak">{feature}</Text>
+                            </li>
+                        ))}
+                    </ul>
+                </Flex>
+            )}
+
+            {project.outcome && (
+                <Text variant="body-default-s" onBackground="neutral-weak">
+                    <Text as="span" variant="label-default-s" onBackground="neutral-strong">Outcome: </Text>
+                    {project.outcome}
+                </Text>
+            )}
+
+            <Flex fillWidth horizontal="space-between" align="center" gap="24" wrap>
+              <Flex align="center" gap="8">
+                 <Avatar src={person.avatar} size="s"/>
+                 <Text variant="body-default-xs" onBackground="neutral-weak">{person.name}</Text>
+              </Flex>
+              <Flex gap="16">
+                  {project.caseStudy && (
+                    <SmartLink
+                      suffixIcon="arrowRight"
+                      style={{ margin: "0", width: "fit-content" }}
+                      href={project.caseStudy}
+                    >
+                      <Text variant="body-default-s">Read case study</Text>
+                    </SmartLink>
+                  )}
+                  {project.link && (
+                    <SmartLink
+                      suffixIcon="arrowUpRightFromSquare"
+                      style={{ margin: "0", width: "fit-content" }}
+                      href={project.link}
+                    >
+                      <Text variant="body-default-s">View project</Text>
+                    </SmartLink>
+                  )}
+              </Flex>
             </Flex>
           </Flex>
         </div>
