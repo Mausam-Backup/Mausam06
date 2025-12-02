f9e38f2fcb158515b880417cf0d6c916c8e5c590
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2025-12-02 19:15:47 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/once-ui/tokens/index.scss";
 import classNames from "classnames";
 
 import { Footer, Header, RouteGuard } from "@/components";
+import SmoothScrolling from "@/components/SmoothScrolling";
 import { baseURL, effects, style } from "@/app/resources";
 
 import { Inter } from "next/font/google";
@@ -92,69 +93,71 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     >
       <ToastProvider>
         <Column style={{ minHeight: "100vh" }} as="body" fillWidth margin="0" padding="0">
-          <Background
-            mask={{
-              cursor: effects.mask.cursor,
-              x: effects.mask.x,
-              y: effects.mask.y,
-              radius: effects.mask.radius,
-            }}
-            gradient={{
-              display: effects.gradient.display,
-              x: effects.gradient.x,
-              y: effects.gradient.y,
-              width: effects.gradient.width,
-              height: effects.gradient.height,
-              tilt: effects.gradient.tilt,
-              colorStart: effects.gradient.colorStart,
-              colorEnd: effects.gradient.colorEnd,
-              opacity: effects.gradient.opacity as
-                | 0
-                | 10
-                | 20
-                | 30
-                | 40
-                | 50
-                | 60
-                | 70
-                | 80
-                | 90
-                | 100,
-            }}
-            dots={{
-              display: effects.dots.display,
-              color: effects.dots.color,
-              size: effects.dots.size as any,
-              opacity: effects.dots.opacity as any,
-            }}
-            grid={{
-              display: effects.grid.display,
-              color: effects.grid.color,
-              width: effects.grid.width as any,
-              height: effects.grid.height as any,
-              opacity: effects.grid.opacity as any,
-            }}
-            lines={{
-              display: effects.lines.display,
-              opacity: effects.lines.opacity as any,
-            }}
-          />
-          <Flex fillWidth minHeight="16"></Flex>
-          <Header />
-          <Flex
-            position="relative"
-            zIndex={0}
-            fillWidth
-            paddingY="l"
-            paddingX="l"
-            horizontal="center"
-            flex={1}
-          >
-            <Flex horizontal="center" fillWidth minHeight="0">
-              <RouteGuard>{children}</RouteGuard>
+          <SmoothScrolling>
+            <Background
+              mask={{
+                cursor: effects.mask.cursor,
+                x: effects.mask.x,
+                y: effects.mask.y,
+                radius: effects.mask.radius,
+              }}
+              gradient={{
+                display: effects.gradient.display,
+                x: effects.gradient.x,
+                y: effects.gradient.y,
+                width: effects.gradient.width,
+                height: effects.gradient.height,
+                tilt: effects.gradient.tilt,
+                colorStart: effects.gradient.colorStart,
+                colorEnd: effects.gradient.colorEnd,
+                opacity: effects.gradient.opacity as
+                  | 0
+                  | 10
+                  | 20
+                  | 30
+                  | 40
+                  | 50
+                  | 60
+                  | 70
+                  | 80
+                  | 90
+                  | 100,
+              }}
+              dots={{
+                display: effects.dots.display,
+                color: effects.dots.color,
+                size: effects.dots.size as any,
+                opacity: effects.dots.opacity as any,
+              }}
+              grid={{
+                display: effects.grid.display,
+                color: effects.grid.color,
+                width: effects.grid.width as any,
+                height: effects.grid.height as any,
+                opacity: effects.grid.opacity as any,
+              }}
+              lines={{
+                display: effects.lines.display,
+                opacity: effects.lines.opacity as any,
+              }}
+            />
+            <Flex fillWidth minHeight="16"></Flex>
+            <Header />
+            <Flex
+              position="relative"
+              zIndex={0}
+              fillWidth
+              paddingY="l"
+              paddingX="l"
+              horizontal="center"
+              flex={1}
+            >
+              <Flex horizontal="center" fillWidth minHeight="0">
+                <RouteGuard>{children}</RouteGuard>
+              </Flex>
             </Flex>
-          </Flex>
-          <Footer />
+            <Footer />
+          </SmoothScrolling>
         </Column>
       </ToastProvider>
     </Flex>
